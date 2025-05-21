1d102e351d1ed54e6c36ba1b3a7fc09ec42be9ee
JEEN-1 <ouralemblaq@gmail.com>
2025-05-21 19:56:25 +0900

    Update 새-클라이언트.bat
    디렉토리 변경

--- 새-클라이언트.bat
+++ 새-클라이언트.bat
@@ -1,3 +1,3 @@
 echo [🖥️] 클라이언트 실행...
-cd /d "C:\Users\oural\Desktop\대면비대면\4학년 1학기\bufschat"
+cd /d "%~dp0.."
 start cmd /k "call mvnw.cmd exec:java -Dexec.mainClass=com.example.bufschat.client.StompChatClient"
