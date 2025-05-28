e7a9397e1a3b617610c9cfb33b9cb91b02f65a93
JEEN-1 <ouralemblaq@gmail.com>
2025-05-28 23:18:55 +0900

    set timezone

--- src/main/java/com/example/bufschat/BufschatApplication.java
+++ src/main/java/com/example/bufschat/BufschatApplication.java
@@ -3,8 +3,18 @@ package com.example.bufschat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BufschatApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BufschatApplication.class, args);
     }
